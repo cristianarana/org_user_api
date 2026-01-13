5fa2030817d570b5997947bb18a6786b77c0ca52
Cristian Arana <cristianarana767@gmail.com>
2026-01-13 10:17:56 -0300

    add base project

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import 'reflect-metadata';
+import Fastify from 'fastify';
+
+const app = Fastify({ logger: true });
+
+app.get('/health', async () => {
+  return { status: 'ok' };
+});
+
+app.listen({ port: 3000, host: '0.0.0.0' });
